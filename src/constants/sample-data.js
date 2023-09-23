a731da28539ab5f80ef3ed918caad2675f623306
Bart <bartoszswierzynski7@gmail.com>
2023-09-23 12:48:55 -0500

    added 3 additional Hiking trails

--- src/constants/sample-data.js
+++ src/constants/sample-data.js
@@ -16,9 +16,7 @@ export const sampleData = [
       address: "8510 Mendenhall Loop Road, Juneau, AK 99801",
       annualVisitors: 1881000,
     },
-    images: [
-      //   "http://dspncdn.com/a1/media/692x/f8/58/77/f85877c41fb6147599886048e3582d47.jpg",
-    ],
+    images: [],
   },
   {
     id: 2,
@@ -34,9 +32,7 @@ export const sampleData = [
       address: "Tioga Rd Hwy 120 & Hwy 140 Yosemite National Park, CA 95389",
       annualVisitors: 3667550,
     },
-    images: [
-      //   "http://dspncdn.com/a1/media/692x/f8/58/77/f85877c41fb6147599886048e3582d47.jpg",
-    ],
+    images: [],
   },
   {
     id: 3,
@@ -52,9 +48,7 @@ export const sampleData = [
       address: "Inyo National Forest 351 Pacu Lane Suite 200 Bishop, CA 93514",
       annualVisitors: 4000000,
     },
-    images: [
-      //   "http://dspncdn.com/a1/media/692x/f8/58/77/f85877c41fb6147599886048e3582d47.jpg",
-    ],
+    images: [],
   },
   {
     id: 4,
@@ -66,13 +60,82 @@ export const sampleData = [
     details: {
       description:
         "The John Muir Trail is a world-renowned long-distance hiking route stretching over 200 miles through California's Sierra Nevada mountains, showcasing stunning alpine landscapes and natural beauty.",
-      website: "https://www.alltrails.com/trail/us/california/mount-whitney-via-mount-whitney-trail",
+      website: "https://www.nps.gov/yose/planyourvisit/jmt.htm",
       address: "Happy Isles Trailhead, Yosemite Valley, Yosemite National Park, CA 95389",
-      annualVisitors: 4000000,
+      annualVisitors: 10000,
       //miles: 219
     },
-    images: [
-      //   "http://dspncdn.com/a1/media/692x/f8/58/77/f85877c41fb6147599886048e3582d47.jpg",
-    ],
+    images: [],
+  },
+  {
+    id: 5,
+    name: "Smith Rock State Park",
+    geolocation: {
+      lat: 44.36018,
+      lon: -121.13884,
+    },
+    details: {
+      description:
+        "Smith Rock, established as a state park in central Oregon in 1960, is widely recognized as the beginning of American sport climbing culture.",
+      website: "https://stateparks.oregon.gov/index.cfm?do=park.profile&parkId=36",
+      address: "9241 NE Crooked River Dr. Terrebonne, OR 97760",
+      annualVisitors: 747000,
+      miles: 3,
+    },
+    images: [],
+  },
+  {
+    id: 6,
+    name: "Pacific Crest Trail",
+    geolocation: {
+      lat: 40.75427,
+      lon: -121.38551,
+    },
+    details: {
+      description:
+        "The Pacific Crest Trail (PCT) offers a transformative long-distance hiking experience along the scenic western border of the United States, spanning from the border of Mexico to Canada, allowing adventurers to connect with the natural beauty of the American wilderness.",
+      website: "https://www.pcta.org/",
+      address: "1323 Club Drive Vallejo, CA 94592",
+      annualVisitors: 400000,
+      // miles: 2650,
+    },
+    images: [],
+  },
+  {
+    id: 7,
+    name: "Appalachian Trail",
+    geolocation: {
+      lat: 44.13554,
+      lon: -72.59428,
+    },
+    details: {
+      description:
+        "The Appalachian Trail (AT) beckons hikers on a legendary journey, traversing the rugged beauty of the Eastern United States from Georgia to Maine, providing an immersive wilderness experience.",
+      website: "https://www.nps.gov/appa/index.htm",
+      address: "Appalachian National Scenic Trail, Killington, VT 05751",
+      annualVisitors: 3000000,
+      // miles: 2198,
+    },
+    images: [],
   },
 ];
+
+/* Object Structure
+{
+  id: 0,
+  name: "",
+  geolocation: {
+    lat: 0,
+    lon: 0,
+  },
+  details: {
+    description:
+      "",
+    website: "",
+    address: "",
+    annualVisitors: 0,
+    // miles: 0,
+  },
+  images: [],
+},
+*/
